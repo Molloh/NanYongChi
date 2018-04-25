ced3a36d6787a747e741e55a0124323fc013d340
arloor <admin@arloor.com>
2018-04-25 14:02:34 +0800

    调用后端，获取openId
    app.globalData中有了openId数据

--- app.js
+++ app.js
@@ -6,12 +6,36 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
+    //获得用户数据，并设置到app.globalData
+    var that = this;
     wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      success: function (res) {
+        if (res.code) {
+          wx.request({
+            url: 'https://nywc.moontell.cn/api/weixin/openid?js_code=' + res.code,
+            method: "get",
+            success: function (openIdRes) {
+              //console.log(openIdRes);
+              // console.info("登录成功返回的openId：" + openIdRes.data.openid);
+
+              // 判断openId是否获取成功
+              if (openIdRes.data.openid != null & openIdRes.data.openid != undefined) {
+                that.globalData.openId = openIdRes.data.openid;
+                console.info("openId： " + that.globalData.openId);
+
+              } else {
+                console.info("获取用户openId失败");
+              }
+            },
+            fail: function (error) {
+              console.info("获取用户openId失败");
+              console.info(error);
+            }
+          })
+        }
       }
-    })
+    });
+    //获得用户数据，并设置到app.globalData-----结束
     // 获取用户信息
     wx.getSetting({
       success: res => {
@@ -34,6 +58,7 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    openId:""
   }
 })
