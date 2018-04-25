b9544e53954a59284991f5bed168ef3df414eb1a
arloor <admin@arloor.com>
2018-04-25 15:29:01 +0800

    获取用户数据——直接根据openid从数据库获取

--- app.js
+++ app.js
@@ -12,16 +12,21 @@ App({
       success: function (res) {
         if (res.code) {
           wx.request({
-            url: 'https://nywc.moontell.cn/api/weixin/openid?js_code=' + res.code,
+            url: 'https://nywc.moontell.cn/api/weixin/memberinfo?js_code=' + res.code,
             method: "get",
             success: function (openIdRes) {
-              //console.log(openIdRes);
-              // console.info("登录成功返回的openId：" + openIdRes.data.openid);
+              
 
               // 判断openId是否获取成功
-              if (openIdRes.data.openid != null & openIdRes.data.openid != undefined) {
-                that.globalData.openId = openIdRes.data.openid;
+              if (openIdRes.data.openId != null & openIdRes.data.openId != undefined) {
+                that.globalData.openId = openIdRes.data.openId;
+                that.globalData.userInfo = openIdRes.data;
                 console.info("openId： " + that.globalData.openId);
+                console.log("app 用户信息：", openIdRes.data);
+
+                if (that.userInfoReadyCallback) {
+                  that.userInfoReadyCallback(openIdRes)
+                }
 
               } else {
                 console.info("获取用户openId失败");
@@ -36,26 +41,26 @@ App({
       }
     });
     //获得用户数据，并设置到app.globalData-----结束
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+    // // 获取用户信息
+    // wx.getSetting({
+    //   success: res => {
+    //     if (res.authSetting['scope.userInfo']) {
+    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    //       wx.getUserInfo({
+    //         success: res => {
+    //           // 可以将 res 发送给后台解码出 unionId
+    //           this.globalData.userInfo = res.userInfo
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //           // 所以此处加入 callback 以防止这种情况
+              // if (this.userInfoReadyCallback) {
+              //   this.userInfoReadyCallback(res)
+              // }
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
   },
   globalData: {
     userInfo: null,
--- pages/index/index.js
+++ pages/index/index.js
@@ -21,34 +21,17 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+      
+    } else{
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          userInfo: res.data
         })
+        console.log("index 用户信息：",this.data.userInfo);
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    
   }
 })
