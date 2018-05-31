f34ea534fc497ea6690725d40d7c6289205c99c5
arloor <admin@arloor.com>
2018-05-31 21:36:07 +0800

    整合我和焦立群在index.js中的代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -37,7 +37,7 @@ Page({
   onLoad: function() {
     let that = this;
     wx.request({
-      url: 'https://nywc.moontell.cn/api/card/details',
+      url: app.apiURL+'/card/details',
       data: {
         maxSeconds: 0,
         minSeconds: 0,
@@ -47,28 +47,27 @@ Page({
         'content-type': 'application/json',
       },
       success: function (res) {
-        console.log(res.data);
+        console.log("卡片详情",res.data);
         that.setData({
           cardInfoList: res.data,
         })
       }
     })
+
+    //获取用户信息（原来的代码无效）
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo
+      })
+    } else {
+      // 由于 请求getinfo接口 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.data
+        })
+        console.log("index 用户信息：", this.data.userInfo);
+      }
+    }
   }
-  // onLoad: function () {
-  //   //获取用户信息（原来的代码无效）
-  //   if (app.globalData.userInfo) {
-  //     this.setData({
-  //       userInfo: app.globalData.userInfo
-  //     })
-  //   } else {
-  //     // 由于 请求getinfo接口 是网络请求，可能会在 Page.onLoad 之后才返回
-  //     // 所以此处加入 callback 以防止这种情况
-  //     app.userInfoReadyCallback = res => {
-  //       this.setData({
-  //         userInfo: res.data
-  //       })
-  //       console.log("index 用户信息：", this.data.userInfo);
-  //     }
-  //   }
-  // }
 })
