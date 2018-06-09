a7a4f15c054f7ba321e0862851e3e1dd5146d2ff
Molloh <mollohchiao@hotmail.com>
2018-06-09 23:59:41 +0800

    update css of page 'add'

--- pages/add/add.js
+++ pages/add/add.js
@@ -17,9 +17,31 @@ domain: 'http://nywcstatic.moontell.cn',
 var app = getApp()
 Page({
   data: {
+    files: [],
     imageObject: {},
     progress:0
   },
+
+  chooseImage: function (e) {
+    var that = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        });
+      }
+    })
+  },
+  previewImage: function (e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
+  },
+
   //事件处理函数
   onLoad: function () {
     // console.log('onLoad')
